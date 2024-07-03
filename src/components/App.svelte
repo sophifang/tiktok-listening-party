842a6a13817b179aa00ce56f476f55136706bb55
Cristina De La Torre <CristinaEsterDeLaTorre@gmail.com>
2024-07-03 13:17:01 -0700

    starter edits

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,11 +1,32 @@
 <script>
-  // Write your JS here, or import other files
+  import Boosting from "./Boosting.svelte";
+  // import DesignADino from "./Design-A-Dino.svelte";
+  // import DinoTrends from "./DinoTrends.svelte";
+
+  // let page = "home"
+  // function change_page(new_page){
+  //   if (new_page == "boosts"){
+  //     page = "boosts"
+  //   } else{
+  //     page = "home"
+  //   }
+  // }
 </script>
 
 <main>
-  <h1>Svelte template</h1>
 
-  <p>Write your HTML here</p>
+  <Boosting/> 
+  
+  <!-- <div class = "home">
+    <img class = "background" src = "Listening Party - resized.png"/>
+    <button>LET'S GO!</button>
+  </div> -->
+  
+
+  <!-- {#if page == "boosts"}
+    <Boosting/>  
+  {/if} -->
+
 </main>
 
 <style>
---
+++ src/components/Boosting.svelte
@@ -0,0 +1,54 @@
+<script>
+
+    import Modal from './BoostingModal.svelte';
+
+    let showModal = false;
+    
+</script>
+  
+<main>
+    <div class = "container">
+        <div class = "listening-screen">
+            <img class = "background" src = "Listening Party - resized.png" />
+        </div>
+
+        <button class="boost-btn" on:click={() => (showModal = true)}> </button>
+
+    </div>
+
+    <Modal bind:showModal>
+    </Modal>
+
+  
+</main>
+  
+<style>
+
+    .container{
+        margin-top: 50px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .listening-screen .background {
+        height: 700px;
+        width: auto;
+    }
+
+    .container .boost-btn {
+        position: absolute;
+        top: 92%;
+        left: 54%;
+        transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        background-color: transparent;
+        color: white;
+        border: none;
+        cursor: pointer;
+        height: 25px;
+        width: 20px;
+        border-radius: 10px;
+        }
+</style>
+
+  
